d7832719ee3b5c2f7415aed8723f68ca288e167e
Maxim Karelov <mkarelov@griddynamics.com>
2016-12-08 15:57:09 +0400

    Add ability to use both aws ec2 link for updating scripts and links to zip repos at github for scripting service

--- components/tonomi-scripts/update-scripts.sh
+++ components/tonomi-scripts/update-scripts.sh
@@ -1,16 +1,46 @@
 #!/bin/sh
 
 read input
-url=`echo "$input" | sed -n "s|{url: [\'\"]\(.*\)[\'\"]}|\1|p"`
-rm -rf repo.zip
-wget -q -O repo.zip "$url" || apk add --update openssl && wget -q -O repo.zip "$url"
-folder=`echo "$url" | cut -d/ -f 5`
-branch=`echo "$url" | sed -n 's/\(.*\)archive\/\(.*\).zip/\2/p'`
-rm -rf ${folder}-${branch}
-unzip repo.zip > /dev/null
-find ${folder}-${branch}/components/tonomi-scripts/scripted-components -name "*.py" -type f -exec chmod +x {} \;
-rm -rf /common /marathon /zookeeper /redis /cassandra /kafka /webui /spark /isp-environment /tw-consumer
-mv ${folder}-${branch}/components/tonomi-scripts/scripted-components/* /
-rm -rf ${folder}-${branch}
+url=`echo "$input" | sed -n "s|{url: [\'\"]\(.*\)[\'\"]}|\1|p"` &&
+rm -f repo.zip repo.tar.gz &&
+
+case "$url" in
+	*github\.com*\.zip)
+		wget -q -O repo.zip "$url" || apk add --update openssl && wget -q -O repo.zip "$url" &&
+		folder=`echo "$url" | cut -d/ -f 5` &&
+		branch=`echo "$url" | sed -n 's/\(.*\)archive\/\(.*\).zip/\2/p'` &&
+
+		if [[ ! -z "$branch" && ! -z "$folder" ]]; then
+			# usage of such links is deprecated
+			rm -rf ${folder}-${branch} &&
+			unzip repo.zip > /dev/null &&
+			find ${folder}-${branch}/components/tonomi-scripts/scripted-components -name "*.py" -type f -exec chmod +x {} \; &&
+			rm -rf /common /marathon /zookeeper /redis /cassandra /kafka /webui /spark /isp-environment /tw-consumer &&
+			mv ${folder}-${branch}/components/tonomi-scripts/scripted-components/* / &&
+			rm -rf ${folder}-${branch}
+		else
+			rm -rf manifests tonomi-scripts &&
+			unzip repo.zip > /dev/null &&
+			find tonomi-scripts/scripted-components -name "*.py" -type f -exec chmod +x {} \; &&
+			rm -rf /common /marathon /zookeeper /redis /cassandra /kafka /webui /spark /isp-environment /tw-consumer &&
+			mv tonomi-scripts/scripted-components/* /
+		fi
+		;;
+
+	*\.tar\.gz*)
+		wget -q -O repo.tar.gz "$url" || apk add --update openssl && wget -q -O repo.tar.gz "$url" &&
+		rm -rf manifest tonomi-scripts &&
+		tar -zxf repo.tar.gz &&
+		find tonomi-scripts/scripted-components -name "*.py" -type f -exec chmod +x {} \; &&
+		rm -rf /common /marathon /zookeeper /redis /cassandra /kafka /webui /spark /isp-environment /tw-consumer &&
+		mv tonomi-scripts/scripted-components/* /
+		;;
+
+	*)
+	  echo "unknown url"
+	  exit 1
+	  ;;
+esac &&
+
 echo "{result: \"$url\"}"
 exit 0
